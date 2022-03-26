6f749c03ac6b159d6e5316406fba578e17aaa940
mostafa <mostafa.18321630@compit.aun.edu.eg>
2022-03-26 19:31:56 +0200

    search and analysis and labs page

--- Thalili/Controllers/SearchController.cs
+++ Thalili/Controllers/SearchController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Thalili.Models;
 namespace Thalili.Controllers
 {
     public class SearchController : Controller
     {
         // GET: Search
-        public ActionResult Index(string searchField)
+        thaliliEntities context = new thaliliEntities();
+        public ActionResult Index(string searchField,int? page)
         {
-            return View();
+            //if(List=="all")
+            //{
+            //    //ViewData["data"] = labs;
+            //    //(IEnumerable<lab>)ViewData["labs"]
+            //}
+            var labs = context.labs.Where(d => d.name.Contains(searchField)).ToList();
+            var analyss= context.medical_analysis.Where(d => d.name.Contains(searchField)).ToList();
+            search_view anal_lab = new search_view(labs,analyss);
+            ViewData["searchField"] = searchField;
+            if (page == null)
+                page = 1;
+            ViewData["page"] = page;
+            return View("lab_analysis",anal_lab);
+        }
+        public ActionResult filters()
+        {
+
         }
     }
 }
