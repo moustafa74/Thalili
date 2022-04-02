46354bc693a3f5474f7044f8699b13eb0eb043d8
Abdullah Abubraik <abdallah2000201@gmail.com>
2022-04-02 19:27:16 +0200

    delete analysis with error

--- Thalili/Controllers/LabDashboardController.cs
+++ Thalili/Controllers/LabDashboardController.cs
@@ -13,11 +13,13 @@ namespace Thalili.Controllers
         int lab_id = 1;
         public ActionResult Orders()
         {
-            return View();
+            var orders = context.sub_order.Where(d => d.analysis_in_lab_Labs_id == lab_id).ToList();
+            
+            return View(orders);
         }
         public ActionResult Analysis()
         {
-
+                
             var analysis_in_lab = context.analysis_in_lab.Where(d => d.Labs_id == lab_id).ToList();
 
             return View(analysis_in_lab);
@@ -27,7 +29,15 @@ namespace Thalili.Controllers
             var edit = context.analysis_in_lab.FirstOrDefault(d => d.Labs_id == lab_id && d.medical_analysis.name == tName);
             edit.price = price;
             context.SaveChanges();
-            return View("Orders");
+            return RedirectToAction("Analysis");
+        }
+        [HttpGet]
+        public ActionResult DeleteThalil(int id)
+        {
+            var deleteitem = context.analysis_in_lab.FirstOrDefault(d => d.Labs_id == lab_id && d.medical_analysis_id == id);
+            context.analysis_in_lab.Remove(deleteitem);
+            context.SaveChanges();
+            return RedirectToAction("Analysis");
         }
         public ActionResult Settings()
         {
