f478815686a44bed50105f5e0ddae708701a1c11
Abdullah Abubraik <abdallah2000201@gmail.com>
2022-05-05 19:46:14 +0200

    analysis search completed

--- Thalili/Controllers/Analysis_and_LabsController.cs
+++ Thalili/Controllers/Analysis_and_LabsController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Thalili.Models;
 
@@ -10,7 +8,8 @@ namespace Thalili.Controllers
     public class Analysis_and_LabsController : Controller
     {
         // GET: Analysis_and_Labs
-        thaliliEntities context = new thaliliEntities();
+        private thaliliEntities context = new thaliliEntities();
+
         public ActionResult Index(int? page)
         {
             var labs = context.labs.ToList();
@@ -21,10 +20,12 @@ namespace Thalili.Controllers
             ViewData["page"] = page;
             return View("lab_analysis", anlysis_labs);
         }
+
         public ActionResult lab(int id)
         {
             return View();
         }
+
         public ActionResult analysis(int id, int? page)
         {
             if (page == null)
@@ -33,9 +34,9 @@ namespace Thalili.Controllers
             var labs = context.analysis_in_lab.Where(d => d.medical_analysis_id == id).ToList();
             return View(labs);
         }
+
         public ActionResult AddToCart(int lab_id, int analysis_id)
         {
-
             if (Session["UserID"] == null)
                 return RedirectToAction("Index", "Login");
             int user_id = (int)Session["UserID"];
@@ -48,75 +49,91 @@ namespace Thalili.Controllers
             context.SaveChanges();
             return RedirectToAction("Index", "Cart");
         }
+
         public ActionResult SearchLab(int AnalysisID, string Labname, int? page)
         {
             if (page == null)
                 page = 1;
             ViewData["page"] = page;
+            var lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == AnalysisID).ToList();
             var analysis_in_lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == AnalysisID && d.lab.name.Contains(Labname)).ToList();
-            return View("analysis",analysis_in_lab);
+            if (analysis_in_lab.Count == 0)
+            {
+                ViewBag.isEmpty = true;
+                ViewData["page"] = 1;
+            }
+            else
+                lab = analysis_in_lab;
+            return View("analysis", lab);
         }
-        
-        public ActionResult Labsfilter (int filterPrice , int filterRating , int? page , int analysis_id)
+
+        public ActionResult Labsfilter(int filterPrice, int filterRating, int? page, int analysis_id)
         {
             var lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id).ToList();
-            int from = 0, to = 0;
+            int from = 0, to = int.MaxValue;
             if (page == null)
                 page = 1;
-            
+
             ViewData["page"] = page;
 
-            if(filterPrice == 1)
+            if (filterPrice == 1)
             {
-                 lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id).ToList();
+                // lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id).ToList();
                 from = 0;
             }
-            else if(filterPrice == 2)
+            else if (filterPrice == 2)
             {
-               // var lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.price >= 50 && d.price <= 100).ToList();
+                // var lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.price >= 50 && d.price <= 100).ToList();
                 from = 50; to = 100;
             }
             else if (filterPrice == 3)
             {
-
-                 lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.price >= 100 && d.price <= 200).ToList();
+                //lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.price >= 100 && d.price <= 200).ToList();
+                from = 100; to = 200;
             }
             else if (filterPrice == 4)
             {
-                 lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.price >= 200 && d.price <= 300).ToList();
+                //lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.price >= 200 && d.price <= 300).ToList();
+                from = 200; to = 300;
             }
             else if (filterPrice == 5)
             {
-                 lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.price >= 300).ToList();
+                //lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.price >= 300).ToList();
+                from = 300; to = int.MaxValue;
             }
 
-            if(filterRating == 5)
+            //Ratings
+            ViewBag.isEmpty = false;
+            var temp = new List<analysis_in_lab>();
+            if (filterRating == 1)
             {
-                 lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.lab.lab_rating <= 5 && d.price >= from && d.price <= to).ToList();
+                temp = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.lab.lab_rating <= 1 && d.price >= from && d.price <= to).ToList();
             }
-            //if (filterRating == 1)
-            //{
-            //    var lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.lab.lab_rating <= 1 && d.price >= from && d.price <= to).ToList();
-            //}
-            //if (filterRating == 1)
-            //{
-            //    var lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.lab.lab_rating <= 1 && d.price >= from && d.price <= to).ToList();
-            //}
-            //if (filterRating == 1)
-            //{
-            //    var lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.lab.lab_rating <= 1 && d.price >= from && d.price <= to).ToList();
-            //}
-            //if (filterRating == 1)
-            //{
-            //    var lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.lab.lab_rating <= 1 && d.price >= from && d.price <= to).ToList();
-            //}
-            //if (filterRating == 1)
-            //{
-            //    var lab = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.lab.lab_rating <= 1 && d.price >= from && d.price <= to).ToList();
-            //}
-            return View("analysis", lab);
+            else if (filterRating == 2)
+            {
+                temp = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.lab.lab_rating > 1 && d.lab.lab_rating <= 2 && d.price >= from && d.price <= to).ToList();
+            }
+            else if (filterRating == 3)
+            {
+                temp = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.lab.lab_rating > 2 && d.lab.lab_rating <= 3 && d.price >= from && d.price <= to).ToList();
+            }
+            else if (filterRating == 4)
+            {
+                temp = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.lab.lab_rating > 3 && d.lab.lab_rating <= 4 && d.price >= from && d.price <= to).ToList();
+            }
+            else if (filterRating == 5)
+            {
+                temp = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.lab.lab_rating > 4 && d.lab.lab_rating <= 5 && d.price >= from && d.price <= to).ToList();
+            }
+            else if (filterRating == 6)
+            {
+                temp = context.analysis_in_lab.Where(d => d.medical_analysis_id == analysis_id && d.price >= from && d.price <= to).ToList();
+            }
 
+            if (temp.Count != 0)
+                lab = temp;
+            else ViewBag.isEmpty = true;
+            return View("analysis", lab);
         }
     }
-
 }
