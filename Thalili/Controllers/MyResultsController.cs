4879217487b627aa46af14d0fdbd75cb1515bdd0
mostafa <mostafa.18321630@compit.aun.edu.eg>
2022-05-06 21:22:42 +0200

    create MyOrders and MyResults controlers and view

---
+++ Thalili/Controllers/MyResultsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Thalili.Controllers
+{
+    public class MyResultsController : Controller
+    {
+        // GET: MyResults
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
